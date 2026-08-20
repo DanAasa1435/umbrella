04aace9827232eb329f3e8ce7c6676c88415d542
DanAasa1435 <danielforhonor99@gmail.com>
2026-08-20 01:15:00 -0400

    Add files via upload

--- wthsdle.js
+++ wthsdle.js
@@ -50,9 +50,9 @@ if (attempts === 3 || attempts === 4) {
 	teacherName.textContent = selectedOption.text;
 	
 	if (selectedOption.text.includes('Mr.')) {
-    profile.src = 'images/maleProfile.jpg'; 
+    profile.src = 'Images/maleProfile.jpg'; 
    } else {
-    profile.src = 'images/femaleProfile.png';  
+    profile.src = 'Images/femaleProfile.png';  
 }
 
    department.textContent = guessedTeacher.department;
