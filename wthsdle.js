3d1e83b233da7bb6b8e04668fa13d84cece34c76
DanAasa1435 <danielforhonor99@gmail.com>
2026-08-23 21:44:49 -0400

    Refactor game logic and update endGame parameters

--- wthsdle.js
+++ wthsdle.js
@@ -15,7 +15,7 @@ const winnerInfo = document.querySelector('#winnerInfo');
 const winnerScreen = document.querySelector('#winnerScreen');
 const loserInfo = document.querySelector('#loserInfo');
 const loserScreen = document.querySelector('#loserScreen');
-let attempts = 0;
+let attempts = 1;
 const maxAttempts = 5;
 
 fetch('teachers.json')
